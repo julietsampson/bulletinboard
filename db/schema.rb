c3f654eb76b409967719de0591c16442c573596f
Digital Humanities Center <DHC@dhc-gf2yaq6lt.fios-router.home>
2022-11-12 12:13:16 -0500

    trying to pull the tags and schedule

--- db/schema.rb
+++ db/schema.rb
@@ -1,44 +1,41 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221029174535) do
+ActiveRecord::Schema.define(version: 2022_10_29_174535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "datetime"
-    t.string   "location"
-    t.text     "description"
-    t.string   "tags",         default: "--- []\n"
-    t.integer  "organizer_id"
+    t.string "location"
+    t.text "description"
+    t.string "tags", default: "--- []\n"
+    t.integer "organizer_id"
+    t.index ["organizer_id"], name: "index_events_on_organizer_id"
   end
-
-  add_index "events", ["organizer_id"], name: "index_events_on_organizer_id", using: :btree
 
   create_table "events_students", id: false, force: :cascade do |t|
     t.integer "student_id", null: false
-    t.integer "event_id",   null: false
+    t.integer "event_id", null: false
+    t.index ["event_id"], name: "index_events_students_on_event_id"
+    t.index ["student_id"], name: "index_events_students_on_student_id"
   end
 
-  add_index "events_students", ["event_id"], name: "index_events_students_on_event_id", using: :btree
-  add_index "events_students", ["student_id"], name: "index_events_students_on_student_id", using: :btree
-
   create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.string   "rating"
-    t.text     "description"
+    t.string "title"
+    t.string "rating"
+    t.text "description"
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
