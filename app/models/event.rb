beaf61d8714faa9e6e12f48ac76b791f15eaf831
azimovaaaa <timazimova@gmail.com>
2022-12-01 12:01:54 -0500

    added more filtering tags for events and removed extra commas from the more about event page

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
     belongs_to :organizers
 
     def self.all_tags
-        return ["Freshman", "Sophomore", "Junior", "Senior", "STEM", "Humanities", "Free Food"]
+        return ["Freshman", "Sophomore", "Junior", "Senior", "STEM", "Humanities", "Free Food", "Academic", "Professional", "Social"]
     end
     def self.with_tags(tags_list,sort_by)
       Event.where('array_length(tags, 1) is NULL OR tags && array[?]', tags_list)
